c1c20b12b6173df58e1d154fcfe8d29a494439e5
Keith Holliday <krh121791@gmail.com>
2017-02-01 12:00:21 -0700

    Added options and updated ui for filters

--- app/src/main/java/com/android/thehollidayinn/kibbl/ui/activities/FiltersActivity.java
+++ app/src/main/java/com/android/thehollidayinn/kibbl/ui/activities/FiltersActivity.java
@@ -5,13 +5,31 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.android.thehollidayinn.kibbl.R;
 
 public class FiltersActivity extends AppCompatActivity {
+
+    private AdapterView.OnItemSelectedListener onItemSelectedListener = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+            // your code here
+            TextView selectedTextView = (TextView) selectedItemView;
+            Log.v("test", selectedTextView.getText().toString());
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parentView) {
+            // your code here
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +43,25 @@ public class FiltersActivity extends AppCompatActivity {
 
     private void setUpView() {
         Spinner spinner = (Spinner) findViewById(R.id.breedSpinner);
-        // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.breeds, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(onItemSelectedListener);
+
+        Spinner ageSpinner = (Spinner) findViewById(R.id.ageSpinner);
+        ArrayAdapter<CharSequence> ageAdpater = ArrayAdapter.createFromResource(this,
+                R.array.age, android.R.layout.simple_spinner_item);
+        ageAdpater.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        ageSpinner.setAdapter(ageAdpater);
+        ageSpinner.setOnItemSelectedListener(onItemSelectedListener);
+
+        Spinner genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this,
+                R.array.gender, android.R.layout.simple_spinner_item);
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
+        genderSpinner.setOnItemSelectedListener(onItemSelectedListener);
     }
 
 }
