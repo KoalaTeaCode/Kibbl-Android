40dea11b73ac14fd15319e6a98ee69706fcc109b
Keith Holliday <krh121791@gmail.com>
2017-02-01 11:40:15 -0700

    Added Firebase analytics and crash reporting

--- app/src/main/java/com/android/thehollidayinn/kibbl/ui/activities/FavoritesActivity.java
+++ app/src/main/java/com/android/thehollidayinn/kibbl/ui/activities/FavoritesActivity.java
@@ -11,10 +11,13 @@ import android.view.View;
 import com.android.thehollidayinn.kibbl.R;
 import com.android.thehollidayinn.kibbl.ui.fragments.FavoritesListFragment;
 import com.android.thehollidayinn.kibbl.ui.fragments.ListContentFragment;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 import java.util.List;
 
 public class FavoritesActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,15 @@ public class FavoritesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fragment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+//        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, id);
+        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "favorites activite");
+        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "page");
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
 
         setUpListFragment();
     }
